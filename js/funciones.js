cff59fcf18b1a13db7292541bff9c13b03243796
jeslev4 <jeslev4@gmail.com>
2015-06-14 17:02:29 -0500

    correcion js

--- js/funciones.js
+++ js/funciones.js
@@ -1,61 +1,26 @@
-var sara=1;
+$(document).ready(function () {
 
-function agregar(){
-    if(sara < 10){
-        var divmaster =document.createElement("div");//divicion mayor
-        var divmaster2=document.createElement("div");//segunda divición
-        var etiqueta=document.createElement("label");//etiquet label
-        var textetiqueta=document.createTextNode("Familia agregada n°"+sara);//texto de label
-        var inputtexto=document.createElement("input");//texbox
-        var divcaja=document.createElement("div");//divicion de la caja
-        var linka=document.createElement("a");//link de referencia
-        var etiquetalink=document.createTextNode("Desde archivo");//texto del link
-        var linkinput=document.createElement("input");//boton de link
-        var sapanlabel=document.createElement("span");//span k no sabemos k hace :P
+            $("#agregaFam").click(function () {
+                if( ($("#panelFam .form-group").length+1) > 12) {
+                    alert("Solo se pueden usar hasta 12 familias");
+                    return false;
+                }
+                var id = ($('#panelFam .form-group').length + 1).toString();
+                var texto = "  <!--Elemento de 1 secuencia-->\
+                    <div class=\"form-group\" >\
+                        <div class=\"col-xs-12\">\
+                          <label>Drosophila Melanogaster</label>\
+                          <input type=\"text\" class=\"form-control\" id=\"lblSeq"+id+"\" name=\"lblSeq"+id+"\" placeholder=\"Secuencia\">\
+                          <div style=\"position:relative;\">\
+                            <a class='btn btn-primary' href='javascript:;'>Desde archivo\
+                              <input type=\"file\" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:\"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)\";opacity:0;background-color:transparent;color:transparent;' name=\"file_source\" size=\"40\"  onchange='$(\"#upload-file-info\").html($(this).val());'>\
+                            </a>\
+                            &nbsp;\
+                            <span class='label label-info' id=\"upload-file-info\"></span>\
+                          </div>\
+                    </div>\
+                  </div> <!--Final de elemento de secuencia-->";
+                $('#panelFam').append(texto);
+            });
 
-        sapanlabel.setAttribute("class","label label-info");
-        sapanlabel.setAttribute("id","upload-file-info"+sara);
-
-        linkinput.setAttribute("type","file");
-        linkinput.setAttribute("style","position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;");
-        linkinput.setAttribute("name","file_source");
-        linkinput.setAttribute("size","40");
-        linkinput.setAttribute("onchange"," ");
-
-        linka.setAttribute("class","btn btn-primary");
-        linka.setAttribute("href","javascript:;");
-
-        divcaja.setAttribute("style","position:relative;");
-
-        inputtexto.setAttribute("type","text");
-        inputtexto.setAttribute("class","form-control");
-        inputtexto.setAttribute("id","lblSeq1"+sara);
-        inputtexto.setAttribute("name","lblSeq1"+sara);
-        inputtexto.setAttribute("placeholder","Secuencia");
-        
-        divmaster2.setAttribute("class","col-xs-12");
-        
-        divmaster.setAttribute("class","form-group");
-
-        a.appendChild(etiquetalink);
-        a.appendChild(linkinput);
-
-        divcaja.appendChild(a);
-        divcaja.appendChild(sapanlabel);
-
-        etiqueta.appendChild(textetiqueta);
-
-        divmaster2.appendChild(etiqueta);
-        divmaster2.appendChild(inputtexto);
-        divmaster2.appendChild(divcaja);
-
-        divmaster.appendChild(divmaster);
-
-        document.getElementById(carmen).appendChild(divmaster);
-
-	sara=sara+1;
-	}
-  else{
-        alert("Se ha creado más de doce secuencias");
-    }
-}
+        });
