d59ca750f636bb8a742860a55d6734ac6355cb04
m0rb1u5 <goesrex@gmail.com>
2015-06-15 00:07:12 -0500

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -39,3 +39,46 @@ $(document).ready(function () {
             });
 
         });
+(function($) {
+    $.fn.checkFileType = function(options) {
+        var defaults = {
+            allowedExtensions: [],
+            success: function() {},
+            error: function() {}
+        };
+        options = $.extend(defaults, options);
+
+        return this.each(function() {
+
+            $(this).on('change', function() {
+                var value = $(this).val(),
+                    file = value.toLowerCase(),
+                    extension = file.substring(file.lastIndexOf('.') + 1);
+
+                if ($.inArray(extension, options.allowedExtensions) == -1) {
+                    options.error();
+                    $(this).focus();
+                } else {
+                    options.success();
+
+                }
+
+            });
+
+        });
+    };
+
+})(jQuery);
+
+$(function() {
+    $('#file_source1').checkFileType({
+        allowedExtensions: ['txt'],
+        success: function() {
+            alert('Exitoso');
+        },
+        error: function() {
+            alert('Error');
+        }
+    });
+
+});
